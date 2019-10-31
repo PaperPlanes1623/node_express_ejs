c8b44f2ca3b6479e586653ae02836fbd05d34a2c
Dylan Taft <dylbronjames@gmail.com>
2019-10-31 16:51:50 -0600

    finished day of the week todo list

--- app.js
+++ app.js
@@ -3,48 +3,31 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Buy food", "Cook food", "Eat food"];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   var today = new Date();
   var currentDay = today.getDay();
-  var day = "";
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  }
 
-  if (currentDay === 0) {
-    day = "Sunday";
-    // res.sendFile(__dirname + "/Weekend.html");
-    res.render("list", { kindOfDay: day })
-  }
-  else if (currentDay === 1) {
-    day = "Monday";
-    // res.sendFile(__dirname + "/weekday.html");
-    res.render("list", { kindOfDay: day });
-  }
-  else if (currentDay === 2) {
-    day = "Tuesday";
-    // res.sendFile(__dirname + "/weekday.html");
-    res.render("list", { kindOfDay: day });
-  }
-  else if (currentDay === 3) {
-    day = "Wednesday";
-    // res.sendFile(__dirname + "/weekday.html");
-    res.render("list", { kindOfDay: day });
-  }
-  else if (currentDay === 4) {
-    day = "Thursday";
-    // res.sendFile(__dirname + "/weekday.html");
-    res.render("list", { kindOfDay: day });
-  }
-  else if (currentDay === 5) {
-    day = "Friday";
-    // res.sendFile(__dirname + "/weekday.html");
-    res.render("list", { kindOfDay: day });
-  }
-  else {
-    day = "Saturday";
-  }
-  res.render("list", { kindOfDay: day });
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { kindOfDay: day, newListItems: items });
 });
+
+app.post("/", function (req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+})
 
 app.listen(3000, function () {
   console.log("Server running on port 3000");
